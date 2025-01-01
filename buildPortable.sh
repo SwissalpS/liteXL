e9905e3f23c0bb6c01d8d8fdbb4965941b9168c1
Luke aka SwissalpS <Luke@SwissalpS.ws>
2025-01-01 19:25:00 +0100

    adjust buildPortable.sh to new parameters

--- buildPortable.sh
+++ buildPortable.sh
@@ -5,4 +5,6 @@ if [ ! -e "src/api/api.h" ]; then
   echo "Please run this script from the root directory of Lite XL."; exit 1
 fi
 
-./build-packages.sh -P -d liteXL -r && mkdir liteXL/user && cp -r user/* liteXL/user/
+rm -fr buildXL;
+./scripts/build.sh -P -r -b buildXL && mv buildXL/lite-xl buildXL/liteXL && mv buildXL/liteXL/lite-xl buildXL/liteXL/liteXL && mkdir buildXL/liteXL/user && cp -r user/* buildXL/liteXL/user/
+
